5671227aeefad5108ecfb4093595a436dff78d07
Mehdi El Harami <mehdi.el-harami@polymtl.ca>
2025-09-18 17:01:30 -0700

    add user real name

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GsupButton } from '../../components/gsup-button/gsup-button';
 import { Router } from '@angular/router';
 import { Degree } from './../../shared/enums/degree';
+import { AuthentificationService } from '@app/services/authentification/authentification-service'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GsupButton],
+  imports: [CommonModule, GsupButton],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   public degree = Object.values(Degree); // mets en liste de string l'enum //
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public auth: AuthentificationService) {}
 
   navigateTo(degree: string): void {
     this.router.navigate([`/${degree.toLowerCase()}`]);

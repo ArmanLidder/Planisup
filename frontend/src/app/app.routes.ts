64120fad70ae302bbfec04887542894bcdd4cf95
Mehdi El Harami <mehdi.el-harami@polymtl.ca>
2025-09-18 18:27:05 -0700

    reorganized paths

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,15 +4,16 @@ import { Login } from './pages/login/login';
 import { AuthGuard } from './guards/auth.guard';
 import { Program } from './pages/program/program';
 import { VerifyPlans } from './pages/verify-plans/verify-plans';
+import { StudyPlan } from './pages/study-plan/study-plan';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'verifyPlans', component: VerifyPlans },
+  { path: 'study-plan', component: StudyPlan },
   { path: 'accueil', component: Home, canActivate: [AuthGuard] },
   { path: ':type', component: Program }, // ex: /maitrise ---> tous les maitirse de genie appraissent
-  { path: ':type/:departement', component: Program }, // ex: /maitrise/genie-info ---> tout les maitrise dans genie info apparissent
-  { path: ':type/:departement/:degree', component: Program }, // ex: /maitrise/genie-info/professionnelle ---> tous les options maitrise dans genie info professionnell aparissent
-  { path: ':type/:departement/:degree/:option', component: Program }, // ex: /maitrise/genie-info/professionnelle/xxx ---> study plan de ali saffiche
+  // { path: ':type/:departement', component: Program }, // ex: /maitrise/genie-info ---> tout les maitrise dans genie info apparissent
+  // { path: ':type/:departement/:degree', component: Program }, // ex: /maitrise/genie-info/professionnelle ---> tous les options maitrise dans genie info professionnell aparissent
+  // { path: ':type/:departement/:degree/:option', component: Program }, // ex: /maitrise/genie-info/professionnelle/xxx ---> study plan de ali saffiche
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 ];

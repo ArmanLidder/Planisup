b83de7818acec26a65e04147a1d87dc66da44e17
Mehdi El Harami <mehdi.el-harami@polymtl.ca>
2025-09-27 16:42:40 -0700

    added chat controller

---
+++ backend-express/app/controllers/chat.controller/chat.controller.ts
@@ -0,0 +1,16 @@
+// import { Request, Response, Router } from "express";
+// import { Service } from "typedi";
+// import { Logger } from "@app/services/logger.service/logger.service";
+
+// @Service()
+// export class ChatController {
+//   public router: Router;
+
+//   constructor(private logger: Logger) {
+//     this.configureRouter();
+//   }
+
+//   private configureRouter(): void {
+//     this.router = Router();
+//   }
+// }

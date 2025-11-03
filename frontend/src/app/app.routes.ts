d3746ba1344a93acb541fcdede48fb9c05c5df20
hb.96.montreal <hamzaboukaftane1996@outlook.fr>
2025-11-03 18:31:53 -0500

    Feature: add student page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { StudyPlan } from '@app/pages/study-plan/study-plan';
 import { Admin } from '@app/pages/admin/admin';
 import { ViewPlan } from '@app/pages/view-plan/view-plan';
 import { ProgramResolver } from './resolvers/program.resolver';
+import { AddStudentPage } from './pages/add-student/add-student';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
@@ -21,7 +22,7 @@ export const routes: Routes = [
   { path: 'study-plan', component: StudyPlan },
   { path: 'view-plan', component: ViewPlan },
   { path: ':type', component: Program },
-  { path: 'add-student', component: StudyPlan },
+  { path: 'add-student', component: AddStudentPage },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 
--- frontend/src/app/pages/add-student/add-student.ts
+++ frontend/src/app/pages/add-student/add-student.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-add-student',
+ selector: 'app-add-student',
   imports: [],
   templateUrl: './add-student.html',
   styleUrl: './add-student.scss'
 })
-export class AddStudent {
+export class AddStudentPage {
 
 }

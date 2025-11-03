d75c825ab07428912e35f25bb2b7f443d7523a61
hb.96.montreal <hamzaboukaftane1996@outlook.fr>
2025-11-03 17:18:57 -0500

    Feature: add registrar menu

--- frontend/src/app/components/gsup-menu/gsup-menu.ts
+++ frontend/src/app/components/gsup-menu/gsup-menu.ts
@@ -64,10 +64,20 @@ export class GsupMenu {
     return this.currentUser?.role === UserRole.Etudiant;
   }
 
+  public isRegistrar(): boolean {
+    return this.currentUser?.role === UserRole.Registrar;
+  }
+
   public isValidationForStudent(): boolean {
     const statusOngoingOrValidated = this.sPS.studyPlan?.status === StudyPlanStatus.VALIDATED
       || this.sPS.studyPlan?.status === StudyPlanStatus.LIVE;
     return this.isStudent() && statusOngoingOrValidated;
+  }
+
+  public redirectAddUserPage(): void {
+    this.isMenuOpen = false;
+    console.log('Redirect to add user page');
+    // this.router.navigate(['/add-student']);
   }
 
   @HostListener('document:click', ['$event'])

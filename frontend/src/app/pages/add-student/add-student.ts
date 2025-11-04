268eec1d2a89cddc9611fd7199cc581620a763ff
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2025-11-04 00:42:29 -0500

    ui fixing

--- frontend/src/app/pages/add-student/add-student.ts
+++ frontend/src/app/pages/add-student/add-student.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { AddStudentForm } from '@app/components/add-student-form/add-student-form';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-add-student',
   imports: [AddStudentForm, MatCardModule],
   standalone: true,
   templateUrl: './add-student.html',
-  styleUrls: ['./add-student.scss']
+  styleUrls: ['./add-student.scss'],
 })
 export class AddStudentPage {
+  protected list = [];
+  protected readonly allPrograms = new BehaviorSubject<Map<string, string>>(new Map());
+  allPrograms$ = this.allPrograms.asObservable();
 
+  private allProgramsOriginal = new Map<string, string>();
+  selectedProgramId: string | null = null;
+  onSearch(event: string): void {
+    /*const search = removeAccents(event.trim().toLowerCase());
+    if (!search) {
+      this.allPrograms.next(new Map(this.allProgramsOriginal));
+      return;
+    }
+    const filteredPrograms = new Map(
+      [...this.allProgramsOriginal].filter(([_, program]) =>
+        removeAccents(program.toLowerCase()).includes(search)
+      )
+    );
+    this.allPrograms.next(filteredPrograms);*/
+  }
+
+  selectProgram(programId: string): void {
+    /*this.isCreatingNew = false;
+    const switchingProgram = this.selectedProgramId && this.selectedProgramId !== programId;
+
+    if (switchingProgram && this.isEditing && this.hasUnsavedChanges()) {
+      this.confirmExit().then((ok) => {
+        if (!ok) return;
+        this.exitEdit();
+        this.finishSelectProgram(programId);
+      });
+      return;
+    }
+    this.finishSelectProgram(programId);*/
+  }
 }
--- frontend/src/app/resolvers/director.resolver.ts
+++ frontend/src/app/resolvers/director.resolver.ts
@@ -7,10 +7,10 @@ import { ApiService } from '@app/services/api/api-service';
 @Injectable({
   providedIn: 'root',
 })
-export class DirectorResolvers implements Resolve<{ directors:User[], coordinators: User[] }> {
+export class DirectorResolvers implements Resolve<{ directors: User[]; coordinators: User[] }> {
   constructor(private readonly apiService: ApiService) {}
 
-  resolve(): Observable<{ directors:User[], coordinators: User[] }> {
+  resolve(): Observable<{ directors: User[]; coordinators: User[] }> {
     return this.apiService.getDirectorsAndCoordinators();
   }
 }
